cc304c306afeaf0ebd36f631bf5885979dbd14eb
unknown <jc.parralejo@iesdoctorbalmis.com>
2024-10-07 16:51:46 +0200

    Añadir todas las clases

---
+++ MiWebBlazor/MiWebBlazor.Shared/Achievement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiWebBlazor.Shared
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ MiWebBlazor/MiWebBlazor.Shared/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiWebBlazor.Shared
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Company { get; set; }
+        public Address? MyAddress { get; set; }
+        public List<Avhievement> MyProperty { get; set; }
+    }
+}
